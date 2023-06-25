426b0e9639a2b85be72138de3a16cbed4bdf124f
sattar <rasoulisattar@gmail.com>
2023-06-26 02:06:10 +0330

    feat: modal is create for creating task

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,47 +3,36 @@ import { API } from '@/services/API'
 import { ChangeEvent, FormEvent, FormEventHandler, useEffect, useState } from 'react'
 import PlusIcon from '../assets/img/plus.png'
 import Image from 'next/image'
+import Modal from '@/components/modal'
+import AddTaskForm from '@/components/AddTaskForm'
 
 export default function Home() {
-  const [title, setTitle] = useState<string>('')
-  const [description, setDescription] = useState<string>('')
+
   const [isComplete, setIsComplete] = useState<string>('')
   const [tasks, setTasks] = useState<string[]>([])
+  const [openModal, setOpenModal] = useState(true)
+
   console.log('tasks', tasks)
   useEffect(() => {
     API.get("/tasks").then((data: any) => setTasks(data.data))
     console.log("e")
   }, [])
 
-  console.log('title', title)
-  console.log('description', description)
-  const handleTitle = (e: ChangeEvent<HTMLInputElement>) => (setTitle(e.target.value))
-  const handleDescription = (e: ChangeEvent<HTMLInputElement>) => (setDescription(e.target.value))
+
+
 
   const handleStatus = (item: any) => {
     API.put(`/tasks/${item.id}`, { id: item.id, title: item.title, description: item.description, status: item.status === "done" ? 'notDone' : "done" })
       .then((result: any) => console.log('result', result))
   }
 
-  function submitForm(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault()
-    API.post("/tasks", { title: title, description: description, status: "idle" }).then((data) => {
-      console.log('data', data)
-      setTitle('')
-      setDescription('')
-    })
-  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-14">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <div className='w-full flex justify-center flex-col items-center'>
-        <form onSubmit={submitForm}>
-          <input type="text" onChange={handleTitle} value={title} />
-          <input type="text" onChange={handleDescription} value={description} />
-          <input type="submit" value="submit" />
-        </form>
         <h3 className='text-xl text-white mb-4'>All Tasks</h3>
-        <div className='bg-white w-1/2 flex flex-col rounded-md shadow-lg shadow-gray-500/40 h-[80vh] relative overflow-y-scroll'>
-          <div className='flex-grow'>
+        <div className='bg-white w-1/2 flex flex-col rounded-md shadow-lg shadow-gray-500/40 h-[80vh] relative'>
+          <div className='flex-grow overflow-y-scroll'>
             {
               tasks.map((item: any) => (
                 <div className={`flex flex-row shadow-lg shadow-gray-500/40 rounded-xl p-2 mx-2 my-1 ${item.status === "done" ? "opacity-50" : "opacity-100"}`} key={item.id}>
@@ -56,11 +45,14 @@ export default function Home() {
               ))
             }
           </div>
-          <div className='bottom-0 justify-center static w-[100%] bg-white opacity-90 '>
-            <button ><Image className='w-[50px]' alt="plusIcon" src={PlusIcon} /></button>
+          <div className='bottom-0 flex justify-center static rounded-xl w-[100%] bg-white opacity-80 '>
+            <button onClick={() => setOpenModal(true)} ><Image className='w-[50px]' alt="plusIcon" src={PlusIcon} /></button>
           </div>
         </div>
 
+        <Modal openModal={openModal} setOpenModal={setOpenModal} >
+          <AddTaskForm />
+        </Modal>
       </div>
     </main>
   )
